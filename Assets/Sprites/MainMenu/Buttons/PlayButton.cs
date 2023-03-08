829dcb54adb891de39f17b61b7e9aade26d46b73
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-03-08 15:09:27 -0500

    main menu fixed

--- Assets/Sprites/MainMenu/Buttons/PlayButton.cs
+++ Assets/Sprites/MainMenu/Buttons/PlayButton.cs
@@ -5,10 +5,9 @@ using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class PlayButton : MainMenuButton
-{   
-    public string startScene;
+{
 
     public override void doStuff() {
-        SceneManager.LoadScene(startScene, LoadSceneMode.Single);
+        SceneManager.LoadScene("Game", LoadSceneMode.Additive);
     }
 }
