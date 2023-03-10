48a8346b0faa16352eb42818efb705fcd84d8825
NotARoomba <70660308+NotARoomba@users.noreply.github.com>
2023-03-10 13:07:17 -0500

    asfasdf

--- Assets/Scripts/RunDialogue.cs
+++ Assets/Scripts/RunDialogue.cs
@@ -7,8 +7,6 @@ public class RunDialogue : MonoBehaviour
 {
     public DialogueRunner dialogueRunner;
     public string dialogueToRun;
-    public bool spacelvl;
-    public bool spaceship;
     public bool hasStarted;
     public bool ending;
 
@@ -22,25 +20,15 @@ public class RunDialogue : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(player != null && player.hasSun) {
-            if (!dialogueRunner.IsDialogueRunning && !ending)
+        if(player != null && ending) {
+            if (!dialogueRunner.IsDialogueRunning && player.hasSun) {
                 dialogueRunner.StartDialogue("Mission_7");
             ending = true;
             gameObject.GetComponent<FadeInScenes>().scene = "Ending";
             gameObject.GetComponent<FadeInScenes>().fadeTime = 1;
-        }
-        else if (spacelvl && hasStarted) {
-            if (!dialogueRunner.IsDialogueRunning) {
-                gameObject.GetComponent<FadeInScenes>().FadeToBlack(true);
-                spacelvl = false;
-            }
-        }
-        else if (spaceship && hasStarted) {
-            if (!dialogueRunner.IsDialogueRunning) {
-                spacelvl = false;
-            }
-        }
 
+            }
+        }
 
     }
 }
